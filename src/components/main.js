cb2eedac42be46fd9ccca0958240c7143f58dcd1
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-09-15 20:01:54 -0400

    Change the shopping card component

--- src/components/main.js
+++ src/components/main.js
@@ -10,7 +10,19 @@ class Main extends Component{
 
   deneme(){
     return _.map(this.props.product, product =>{
-       return <div key={product.id}>{product.text}</div>
+       return(
+        <div class="card" key={product.id}>
+          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap"/>
+
+          <div class="card-body">
+              <h4 class="card-title">{product.text}</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button>Add to cart</button>
+          </div>
+        </div>
+       )
+
+
     })
   }
 

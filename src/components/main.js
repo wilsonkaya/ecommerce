af2fd80ba04ad38b45c320cc722201d82f379995
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-09-17 13:51:15 -0400

    Add order component and re shape the main component to show new features of products

--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import * as actions from '../actions/index'
 import _ from "lodash"
+import Order from './order'
 
 class Main extends Component{
   componentWillMount(){
@@ -15,8 +16,10 @@ class Main extends Component{
           <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap"/>
 
           <div class="card-body">
-              <h4 class="card-title">{product.text}</h4>
+              <h4 class="card-title">{product.name}</h4>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p>{product.text}</p>
+              <p>{product.price}</p>
               <button>Add to cart</button>
           </div>
         </div>
@@ -30,13 +33,13 @@ class Main extends Component{
     return(
       <div>
         {this.deneme()}
+        <Order/>
       </div>
     )
   }
 }
 
 function mapStateToProps(state){
-  console.log("this is the component", state.product.product)
   return {product: state.product.product}
 }
 export default connect(mapStateToProps, actions) (Main)
